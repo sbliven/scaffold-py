70bf44f3e862921c85f38931313383fdb7fb3f69
Spencer Bliven <spencer.bliven@gmail.com>
2019-03-04 09:11:03 +0100

    Store version number within package

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Load the version number from __init__.py
+__version__ = "Undefined"
+for line in open('scaffold/__init__.py'):
+    if (line.startswith('__version__')):
+        exec(line.strip())
+
 config = {
     'description': 'Simple project scaffolding for Python',
     'author': 'Aaron Stannard',
     'url': 'https://github.com/Aaronontheweb/scaffold-py',
     'download_url': 'https://github.com/Aaronontheweb/scaffold-py/archive/v0.1.5.tar.gz',
     'author_email': 'aaron@stannardlabs.com',
-    'version': '0.1.5',
+    'version': __version__,
     'install_requires': ['nose'],
     'packages': ['scaffold'],
     'scripts': [],
